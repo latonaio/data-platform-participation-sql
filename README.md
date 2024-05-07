8bf5ec2d75afe91c382fbe45d542e62fd5bd9b05
misupopo <kokorononakaniitumo@yahoo.co.jp>
2024-05-07 16:35:59 +0900

    no message

--- README.md
+++ README.md
@@ -1,6 +1,6 @@
 # data-platform-participation-sql 
 
-data-platform-participation-sql は、データ連携基盤において、参加データを維持管理するSQLテーブルを作成するためのレポジトリです。  
+data-platform-participation-sql は、データ連携基盤において、参加(権利)データを維持管理するSQLテーブルを作成するためのレポジトリです。  
 
 ## 前提条件  
 data-platform-participation-sql は、データ連携にあたり、API を利用し、本レポジトリ の sql 設定ファイルの内容は、下記 URL の API 仕様を前提としています。  
@@ -10,8 +10,8 @@ https://api.XXXXXXXX.com/api/OP_API_XXXXXXX_XXX/overview
 
 data-platform-participation-sql には、sqlの設定ファイルとして、以下のファイルが含まれています。    
 
-* data-platform-participation-sql-header-data.sql（データ連携基盤 参加 - ヘッダデータ）
-* data-platform-participation-sql-header-doc-data.sql（データ連携基盤 参加 - ヘッダ文書データ）
+* data-platform-participation-sql-header-data.sql（データ連携基盤 参加(権利) - ヘッダデータ）
+* data-platform-participation-sql-header-doc-data.sql（データ連携基盤 参加(権利) - ヘッダ文書データ）
 
 ## MySQLのセットアップ / Kubernetesの設定 / SQLテーブルの作成方法
 MySQLのセットアップ / Kubernetesの設定 / 具体的なSQLテーブルの作成方法、については、[mysql-kube](https://github.com/latonaio/mysql-kube)を参照ください。  
--- data-platform-participation-sql-header-data.sql
+++ data-platform-participation-sql-header-data.sql
@@ -6,13 +6,15 @@ CREATE TABLE `data_platform_participation_header_data`
   `Participator`                          int(12) NOT NULL,
   `ParticipationObjectType`               varchar(40) NOT NULL,
   `ParticipationObject`                   int(16) NOT NULL,
+  `Attendance`                            int(20) DEFAULT NULL,
   `CreationDate`                          date NOT NULL,
   `CreationTime`                          time NOT NULL,
   `IsCancelled`                           tinyint(1) DEFAULT NULL,
 
     PRIMARY KEY (`Participation`),
 
-    CONSTRAINT `DPFMParticipationHeaderDataParticipator_fk` FOREIGN KEY (`Participator`) REFERENCES `data_platform_business_partner_general_data` (`BusinessPartner`)
+    CONSTRAINT `DPFMParticipationHeaderDataParticipator_fk` FOREIGN KEY (`Participator`) REFERENCES `data_platform_business_partner_general_data` (`BusinessPartner`),
+    CONSTRAINT `DPFMParticipationHeaderDataAttendance_fk` FOREIGN KEY (`Attendance`) REFERENCES `data_platform_attendance_header_data` (`Attendance`)
 
 ) ENGINE = InnoDB
   DEFAULT CHARSET = utf8mb4;
